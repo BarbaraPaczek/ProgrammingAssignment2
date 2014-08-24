1957d2d880b86489cec0bceb770ad831ab24197e
BarbaraPaczek <barbarapaczek@Barbaras-MacBook-Air.local>
2014-08-24 22:41:26 +0200

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The aim of these two functions is to use cached inverted matrix when possible.
 
-## Write a short comment describing this function
+## This function creates a special matrix. The result of thsi function will be used as an argument 
+##to the next funxtion, cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  
+  ## This function sets new value to a matrix.
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  
+  ## This function returs the value of x.
+  get <- function() x
+  
+  ## This function assigns the value of solve to s. 
+  setsolve <- function(solve) s <<- solve
+  
+  ## This function returs the value of s, to which inverted matrix is assigned.
+  getsolve <- function() s
+  
+  ## The function makeCacheMatrix returns the list of four functions defined above.
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the special matrix, created in the function makeCacheMatrix.
+## The function will use the cache of the inverted matrix if possible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## The cached inverted matrix is returned if it exists.
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  
+  ## If the cached inverted matrix does not exist, the function restores the soacial matrix and 
+  ## inverts it.
+  data <- x$get()
+  s <- solve(data, ...)
+  
+  ## The result is cached so that it can be used in the future.
+  x$setsolve(s)
+  s
 }
